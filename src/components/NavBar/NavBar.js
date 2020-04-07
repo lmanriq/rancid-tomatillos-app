837235cb1082dd9230f270e41ff907b946645b20
Dan Reardon <dreardon1021@gmail.com>
2020-04-07 15:57:09 -0600

    Add general nav bar layout
    Co-authored-by: Lili Manrique <liliana.manrique1@gmail.com>
    Co-authored-by: Cody Smith <monstaro32@gmail.com>
    Co-authored-by: Trond240 <49410633+Trond240@users.noreply.github.com>

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,12 +5,14 @@ import { NavLink } from 'react-router-dom'
 const NavBar = () => {
     return (
         <section className="nav-bar">
-            <h1 className="site-heading">Rancid Tomatillos</h1>
-            <img
-                alt="tomato logo"
-                className="icon"
-                src="/images/tomato.svg"
-            />
+            <div className="header-icon-container">
+                <h1 className="site-heading">Rancid Tomatillos</h1>
+                <img
+                    alt="tomato logo"
+                    className="icon"
+                    src="/images/tomato.svg"
+                />
+            </div>
             <div className="nav-login-div">
                 <h4 className="user-name">Welcome! Please Login</h4>
                 <NavLink to="/login" className="nav-login-button">Login</NavLink>
