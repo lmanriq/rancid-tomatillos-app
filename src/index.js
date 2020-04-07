026346817683f36a212941cf2002d55dae11a518
Dan Reardon <dreardon1021@gmail.com>
2020-04-07 16:12:37 -0600

    Add imports and Providers for Redux

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from '../src/components/App/App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, composeWithDevtools)
 
 const router = <BrowserRouter><App></App></BrowserRouter>
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
     {router}
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>
+  
+  ,
   document.getElementById('root')
 );
 
