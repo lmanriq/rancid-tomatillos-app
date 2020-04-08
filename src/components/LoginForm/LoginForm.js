76f4039d15bab0bdbc3cbeebfc3b82fd6fe3b37b
Trond Makonese <trondation@gmail.com>
2020-04-08 13:52:19 -0600

    User can now login
    Co-authored-by: Daniel Reardon <dreardon1021@gmail.com>
    Co-authored-by: Lili Manrique <liliana.manrique1@gmail.com>
    Co-authored-by: Cody Smith <monstaro32@gmail.com>

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,26 +1,66 @@
 import React, { Component } from "react";
 import './LoginForm.css';
+import { connect } from 'react-redux';
+import { loginUser } from '../../actions'
+// {email: <String>, password: <String>}
 
 class LoginForm extends Component {
   constructor() {
     super();
     this.state = {
-
+      email: '',
+      password: ''
     }
   }
 
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  submitLogin(e) {
+    e.preventDefault();
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v1/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email: this.state.email, password: this.state.password})
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.err(err.message))
+  }
+
   render() {
-    return(
-      <section class="login-container">
+        return(
+      <section className="login-container">
         <form>
           <h2>Login</h2>
-          <input type="email" placeholder="Email"/>
-          <input type="password" placeholder="Password"/>
-          <button class="login-button">Login</button>
+          <input
+            onChange={(e) => this.handleChange(e)}
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={this.state.email}/>
+          <input
+            onChange={(e) => this.handleChange(e)}
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={this.state.password}/>
+          <button onClick={e => this.submitLogin(e)} type="button" className="login-button">Login</button>
         </form>
       </section>
     )
   }
 }
 
-export default LoginForm
+const mapDispatchToProps = dispatch => ({
+  loginUser: user => dispatch(loginUser(user))
+})
+
+const mapStateToProps = state => ({
+  
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm)
