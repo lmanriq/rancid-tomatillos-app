0381933587313f019b8f69a1087b6060e9fc7716
Trond Makonese <trondation@gmail.com>
2020-04-08 17:35:36 -0600

    remove console log

--- src/reducers/moviesList.js
+++ src/reducers/moviesList.js
@@ -2,7 +2,7 @@ export const moviesList = (state = [], action) => {
   switch (action.type) {
     case 'LOAD_MOVIES':
       console.log()
-      return [...state, ...action.movies];
+      return [...action.movies];
     default:
       return state;
   }
