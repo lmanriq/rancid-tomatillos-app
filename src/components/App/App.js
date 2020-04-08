bd75b253313fe971075e583e79030c8fef7b949b
Trond Makonese <trondation@gmail.com>
2020-04-08 17:32:30 -0600

    remove white space

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,6 @@ import NavBar from '../NavBar/NavBar';
 import LoginPage from '../LoginPage/LoginPage';
 import { Route, Switch } from 'react-router-dom';
 
-
 const App = () => {
     return (
       <div className="App">
