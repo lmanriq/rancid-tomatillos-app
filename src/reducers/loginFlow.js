022afd3425393f0946b9f32bc015de8af592655e
Trond Makonese <trondation@gmail.com>
2020-04-08 16:33:18 -0600

    add logout user to login flow reducer

--- src/reducers/loginFlow.js
+++ src/reducers/loginFlow.js
@@ -2,6 +2,8 @@ export const loginFlow = (state = {}, action) => {
   switch(action.type) {
     case 'LOGIN_USER':
       return {...state, ...action.user}
+    case 'LOGOUT_USER':
+      return {}
     default:
       return state;
   }
