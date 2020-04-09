628e809817f584ec14a5074dbf6567308d9d5092
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-08 21:38:11 -0600

    Delete comments

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -14,15 +14,6 @@ class MovieDetails extends Component {
   }
 
   render() {
-
-//     {id: 1, title: "Bloodshot", poster_path: "https://image.tmdb.org/t/p/original//8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg", backdrop_path: "https://image.tmdb.org/t/p/original//ocUrMYbdjknu2TwzMHKT9PBBQRw.jpg", release_date: "2020-03-05", …}
-// id: 1
-// title: "Bloodshot"
-// poster_path: "https://image.tmdb.org/t/p/original//8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg"
-// backdrop_path: "https://image.tmdb.org/t/p/original//ocUrMYbdjknu2TwzMHKT9PBBQRw.jpg"
-// release_date: "2020-03-05"
-// overview: "After he and his wife are murdered, marine Ray Garrison is resurrected by a team of scientists. Enhanced with nanotechnology, he becomes a superhuman, biotech killing machine—'Bloodshot'. As Ray first trains with fellow super-soldiers, he cannot recall anything from his former life. But when his memories flood back and he remembers the man that killed both him and his wife, he breaks out of the facility to get revenge, only to discover that there's more to the conspiracy than he thought."
-// average_rating: 5
     const { movie } = this.state;
     console.log(movie)
     const backgroundImage = {
