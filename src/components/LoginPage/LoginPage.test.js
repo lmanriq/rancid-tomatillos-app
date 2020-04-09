7e8d3833ec60d3b154731135cdbf2074a6d86e66
monstaro <monstaro32@gmail.com>
2020-04-09 16:55:27 -0600

    Tests for correct renders

--- src/components/LoginPage/LoginPage.test.js
+++ src/components/LoginPage/LoginPage.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import LoginPage from './LoginPage';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../../reducers';
 
 describe('Login Page', () => {
   it('Should render what we expect', () => {
-    
+    const store = createStore(rootReducer)
+    const { getByText } = render (
+      <Provider store = {store}>
+        <Router>
+          <LoginPage />
+        </Router>
+      </Provider>
+    )
+    expect(getByText('Movie Of The Day')).toBeInTheDocument()
   })
 })
