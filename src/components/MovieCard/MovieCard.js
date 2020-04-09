2aff386a7bd5b5b544542821a8702ccce1b68fa2
monstaro <monstaro32@gmail.com>
2020-04-09 13:50:20 -0600

    Adds toFixed

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -7,24 +7,17 @@ const MovieCard = (props) => {
   const filledStars = Array(numStars).fill(<img className = "star" src = "/images/star-green.svg" alt = "green star" />);
   const emptyStars = Array(10 - numStars).fill(<img className = "star" src = "/images/star-clear-outline.svg" alt = "empty star" />);
   const stars = filledStars.concat(emptyStars)
-
-    //  key={movie.id}
-    //       title={movie.title}
-    //       posterImage={movie.poster_path}
-    //       backdropImage={movie.backdrop_path}
-    //       releaseDate={movie.release_date}
-    //       overview={movie.overview}
-    //       averageRating={movie.average_rating}
+  
   return (
     
   <section className="movie-card">
-    <img 
+    <img
       alt="movie poster"
-      className="movie-poster" 
-      src={props.posterImage} 
+      className="movie-poster"
+      src={props.posterImage}
       />
     <h3>{props.title}</h3>
-    <p>Avg. Rating: {props.averageRating}</p>
+    <p>Avg. Rating: {props.averageRating.toFixed(1)}</p>
     <section className="rate-movie">
       {stars}
     </section>
