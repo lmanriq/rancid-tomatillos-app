2ef8e624ecd9498aee350740246b67569d20e50c
monstaro <monstaro32@gmail.com>
2020-04-09 16:56:34 -0600

    Changes display

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -19,8 +19,8 @@ class MovieDetails extends Component {
     const backgroundImage = {
       backgroundImage: `url(${movie.backdrop_path})`
     }
-    const numStars = Math.ceil(this.props.averageRating);
-    const filledStars = Array(numStars).fill("images/star-green.svg");
+    const numStars = Math.ceil(this.state.movie.average_rating);
+    const filledStars = Array(numStars).fill("/images/star-green.svg");
     const emptyStars = Array(10 - numStars).fill("/images/star-clear-outline.svg");
     const stars = filledStars.concat(emptyStars).map((star, index) => {
       return (
