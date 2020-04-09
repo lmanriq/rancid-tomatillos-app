19a2da9dc76e4992bd50b41f374c9ae896b6af9c
monstaro <monstaro32@gmail.com>
2020-04-09 14:19:16 -0600

    Render tests

--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import LoginForm from './LoginForm';
+// import { fetchRatings, postUser } from '../../apicalls';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import rootReducer from '../../reducers';
+import { createStore } from 'redux';
+// jest.mock('../../apiCalls');
 
 describe('Login Form', () => {
-  it('Should render what we expect', () => {
-    
+  it('Should render what we expect', async () => {
+    const store = createStore(rootReducer)
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <Router>
+          <LoginForm />
+        </Router>
+      </Provider>
+    )
   })
 })
