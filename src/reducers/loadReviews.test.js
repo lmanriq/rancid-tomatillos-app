a85d7defbe5a344dbf0866901a618acdbd47da61
Trond Makonese <trondation@gmail.com>
2020-04-12 14:17:26 -0600

    Test for clearing reviews now passes.

--- src/reducers/loadReviews.test.js
+++ src/reducers/loadReviews.test.js
@@ -1,4 +1,5 @@
 import { loadReviews } from './loadReviews';
+import { clearReviews } from '../actions';
 
 describe('loadReviews', () => {
   it('should return the initial state', () => {
@@ -46,7 +47,28 @@ describe('loadReviews', () => {
   })
   
   it('when receiving  CLEAR_REVIEWS action, it clear out all reviews in the array', () => {
+    const sampleAction = {
+        type: 'CLEAR_REVIEWS',
+        reviews: [
+            {
+              movie_id: 1,
+              rating: 4.5
+            },
+            {
+              movie_id: 2,
+              rating: 10
+            },
+            {
+              movie_id: 3,
+              rating: 8
+            }
+            ]
+         }
 
+    const sampleClearReviews = []
+
+    const result = loadReviews([], sampleAction)
+    expect(result).toEqual(sampleClearReviews)
   })
 
   it('when receiving  ADD_REVIEWS action, it should add a moview review to the excisting array of movie reviews', () => {
