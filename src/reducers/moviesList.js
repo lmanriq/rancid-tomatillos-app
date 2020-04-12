da49e9bcc71d1cbec3f416b4c7f1385aad122e32
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-12 10:43:38 -0600

    Add SORT_MOVIES to movieList reducer

--- src/reducers/moviesList.js
+++ src/reducers/moviesList.js
@@ -1,8 +1,19 @@
 export const moviesList = (state = [], action) => {
   switch (action.type) {
     case 'LOAD_MOVIES':
-      console.log()
       return [...action.movies];
+    case 'SORT_MOVIES':
+      const stateCopy = [...state];
+      if (action.option === 'release-date') {
+        return stateCopy.sort((a, b) => {
+          return a.release_date - b.release_date
+        })
+      } else if (action.option === 'alphabetical'){
+        return stateCopy.sort((a, b) => {
+          return a.title < b.title;
+        })
+      }
+      break;
     default:
       return state;
   }
