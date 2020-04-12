73218f037a8a05399b0075a53d17f4d3d06113a5
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-12 13:14:59 -0600

    Updates

---
+++ precache-manifest.bdb74fb37514b78ae3a416580b9a6e12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db812116e650997475e0bb70bfebb278",
+    "url": "/rancid-tomatillos-app/index.html"
+  },
+  {
+    "revision": "2f841155768895f1e800",
+    "url": "/rancid-tomatillos-app/static/css/main.0dc2c779.chunk.css"
+  },
+  {
+    "revision": "f19027556a2d7e0a348f",
+    "url": "/rancid-tomatillos-app/static/js/2.a5a532fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rancid-tomatillos-app/static/js/2.a5a532fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f841155768895f1e800",
+    "url": "/rancid-tomatillos-app/static/js/main.d0eeee5e.chunk.js"
+  },
+  {
+    "revision": "aed63fe0d632300efbdc",
+    "url": "/rancid-tomatillos-app/static/js/runtime-main.6e53268c.js"
+  }
+]);
