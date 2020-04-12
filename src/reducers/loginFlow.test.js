5c9f558ffbbb84990f59adf5d1cd0a6d9801eb9a
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-12 10:13:51 -0600

    Pass login user test

--- src/reducers/loginFlow.test.js
+++ src/reducers/loginFlow.test.js
@@ -7,20 +7,22 @@ describe('loginFlow', () => {
     expect(result).toEqual(expectedResult)
   })
   it('when receiving LOGIN_USER action, it should return the user object', () => {
-    // const sampleAction = {
-    //   type: 'ADD_TODO',
-    //   text: 'Fire your mom',
-    //   completed: false,
-    //   id: 1
-    // }
-    // const sampleTodo = {
-    //   text: 'Fire your mom',
-    //   completed: false,
-    //   id: 1
-    // }
-    // const expectedResult = [sampleTodo];
-    // const result = todosReducer([], sampleAction);
-    // expect(result).toEqual(expectedResult)
+    const sampleAction = {
+      type: 'LOGIN_USER',
+      user: {
+        id: 1,
+        name: 'Alan',
+        email: 'alan@turing.io'
+      }
+    }
+
+    const sampleUser = {
+      id: 1,
+      name: 'Alan',
+      email: 'alan@turing.io'
+    }
+    const result = loginFlow({}, sampleAction);
+    expect(result).toEqual(sampleUser);
   })
   it('when receiving LOGOUT_USER action, it should return an empty', () => {
     // const sampleAction = {
