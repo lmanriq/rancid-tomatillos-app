7ceec5251975ffff86fd99bc16a3917e9235c778
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-12 10:08:36 -0600

    Write imports and outline of loginFlow tests

---
+++ src/reducers/loginFlow.test.js
@@ -0,0 +1,41 @@
+import { loginFlow } from './loginFlow';
+
+describe('loginFlow', () => {
+  it('should return the initial state', () => {
+    // const expectedResult = [];
+    // const result = todosReducer(undefined, {});
+    // expect(result).toEqual(expectedResult)
+  })
+  it('when receiving LOGIN_USER action, it should return the user object', () => {
+    // const sampleAction = {
+    //   type: 'ADD_TODO',
+    //   text: 'Fire your mom',
+    //   completed: false,
+    //   id: 1
+    // }
+    // const sampleTodo = {
+    //   text: 'Fire your mom',
+    //   completed: false,
+    //   id: 1
+    // }
+    // const expectedResult = [sampleTodo];
+    // const result = todosReducer([], sampleAction);
+    // expect(result).toEqual(expectedResult)
+  })
+  it('when receiving LOGOUT_USER action, it should return an empty', () => {
+    // const sampleAction = {
+    //   type: 'ADD_TODO',
+    //   text: 'Fire your mom',
+    //   completed: false,
+    //   id: 1
+    // }
+    // const sampleTodo = {
+    //   text: 'Fire your mom',
+    //   completed: false,
+    //   id: 1
+    // }
+    // const expectedResult = [sampleTodo];
+    // const result = todosReducer([], sampleAction);
+    // expect(result).toEqual(expectedResult)
+  })
+})
