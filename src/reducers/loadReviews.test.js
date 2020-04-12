9babc789677d7894880736ba10a6aaa88bf2a926
Trond Makonese <trondation@gmail.com>
2020-04-12 14:09:10 -0600

    Load reviews in reducers test npow passes.

--- src/reducers/loadReviews.test.js
+++ src/reducers/loadReviews.test.js
@@ -8,7 +8,41 @@ describe('loadReviews', () => {
   })
 
   it('when receiving  LOAD_REVIEWS action, it should return an array of all movie reviews', () => {
+    const sampleAction = {
+        type: 'LOAD_REVIEWS',
+        reviews: [
+        {
+          movie_id: 1,
+          rating: 4.5
+        },
+        {
+          movie_id: 2,
+          rating: 10
+        },
+        {
+          movie_id: 3,
+          rating: 8
+        }
+        ]
+      }
 
+      const sampleReviews = [
+        {
+            movie_id: 1,
+            rating: 4.5
+          },
+          {
+            movie_id: 2,
+            rating: 10
+          },
+          {
+            movie_id: 3,
+            rating: 8
+          }
+      ]
+     
+      const result = loadReviews([], sampleAction);
+      expect(result).toEqual(sampleReviews);
   })
   
   it('when receiving  CLEAR_REVIEWS action, it clear out all reviews in the array', () => {
