1bc46f52acaa16ee1f85c5a2d935281883b2734f
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-12 11:01:30 -0600

    Fix sort function for alphabetical

--- src/reducers/moviesList.js
+++ src/reducers/moviesList.js
@@ -3,17 +3,22 @@ export const moviesList = (state = [], action) => {
     case 'LOAD_MOVIES':
       return [...action.movies];
     case 'SORT_MOVIES':
-      const stateCopy = [...state];
+      let stateCopy = [...state];
       if (action.option === 'release-date') {
-        return stateCopy.sort((a, b) => {
+        stateCopy.sort((a, b) => {
           return a.release_date - b.release_date
         })
       } else if (action.option === 'alphabetical'){
-        return stateCopy.sort((a, b) => {
-          return a.title < b.title;
+        stateCopy.sort((a, b) => {
+          if (a.title < b.title) {
+            return -1;
+          } else if (a.title < b.title) {
+            return 1;
+          }
         })
       }
-      break;
+      console.log(stateCopy);
+      return stateCopy;
     default:
       return state;
   }
